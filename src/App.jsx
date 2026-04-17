9e114d6ebce72a49d041a3dfedd3a5f9f0527e15
Asharf Osama <2300109@dhic.edu.eg>
2026-04-17 17:51:43 +0200

    Add routing and components for YouTube clone; implement Navbar, Feed, VideoDetails, ChannelDetails, and SearchFeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Box from "@mui/material/Box";
 
-
+import {
+  Navbar,
+  Feed,
+  VideoDetails,
+  ChannelDetails,
+  SearchFeed,
+} from "./component";
 function App() {
- 
-
   return (
     <>
-    
+      <BrowserRouter>
+        <Box>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route path="/video/:id" element={<VideoDetails />} />
+            <Route path="/channel/:id" element={<ChannelDetails />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/component/index.jsx
@@ -0,0 +1,10 @@
+
+export { default as Feed } from "./Feed";
+export { default as VideoDetails } from "./VideoDetails";
+export { default as ChannelDetails } from "./ChannelDetails";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as Navbar } from "./Navbar";
+
+
+
+
