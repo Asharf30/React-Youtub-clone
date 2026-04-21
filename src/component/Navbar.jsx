19e56fcf0364abf2bdfdedf71b64e19500199f8f
Asharf Osama <2300109@dhic.edu.eg>
2026-04-21 17:32:53 +0200

    Add SearchBar component and integrate it into Navbar; update TypeScript version in package files and enhance Navbar styling

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,18 +2,25 @@ import { Link } from "react-router-dom";
 import Stack from "@mui/material/Stack";
 
 import { logo } from "../ulites.jsx/constants";
+import SearchBar from "./SearchBar";
 
 function Navbar() {
   return (
     <Stack
       direction="row"
-      alignItems="center"
-    
-      p={2}
-      sx={{position : "sticky", top: 0, justifyContent: "space-between" ,}}
-  
+      sx={{
+        padding: 2,
+        alignItems: "center",
+        position: "sticky",
+        top: 0,
+        justifyContent: "space-between",
+        background: "#000",
+      }}
     >
-
+      <Link to="/">
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
     </Stack>
   );
 }
---
+++ src/component/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import Search from "@mui/icons-material/Search";
+
+function SearchBar() {
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        boxShadow: "none",
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+}
+
+export default SearchBar;
