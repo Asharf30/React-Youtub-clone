b52ddd78dc4e7f4c01b10601cb53357eb6b62bfe
Asharf Osama <2300109@dhic.edu.eg>
2026-04-23 17:38:26 +0200

    Refactor Videos component to accept props and render video and channel cards dynamically

--- src/component/videos.jsx
+++ src/component/videos.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
-function Videos() {
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Videocard from "./Videocard";
+import ChannelCard from "./ChannelCard";
+function Videos({ videos }) {
   return (
-    <div>videos</div>
-  )
+    <Stack
+      sx={{
+        justifyContent: "flex-start",
+        gap: 2,
+        flexDirection: "row",
+        flexWrap: "wrap",
+      }}
+    >
+      {videos.map((item, index) => (
+        <Box key={index} sx={{ color: "white" }}>
+          {item.id.videoId && <Videocard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetails={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 }
 
-export default Videos
+export default Videos;
